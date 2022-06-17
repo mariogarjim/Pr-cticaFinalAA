c9d936d1af7ff7bff0070e512bd545bf073e7796
CodeBoy-source <briansena23@hotmail.com>
2022-06-17 20:07:56 +0200

    Read All Files at once
    This function will reduce noise in main.py

--- main.py
+++ main.py
@@ -4,61 +4,48 @@ import pandas as pd
 
 #Leemos los datos de los distintos archivos
 affirdd = rd.load_affirm_data()
-#affirdd.head()
 affirdt = rd.load_affirm_target()
-affirdt.head()
-
 
 #assert(affirdd.shape[0] == affirdt.shape[0])
-
 condd = rd.load_cond_data()
-condd.head()
 condt = rd.load_cond_target()
-condt.head()
 
 doubtqd = rd.load_doubtq_data()
-doubtqd.head()
 doubtqt = rd.load_doubtq_target()
-doubtqt.head()
 
 emphd = rd.load_emphasis_data()
-emphd.head()
 empht = rd.load_emphasis_target()
-empht.head()
 
 negd = rd.load_neg_data()
-negd.head()
 negt = rd.load_neg_target()
-negt.head()
 
 reld = rd.load_rel_data()
-reld.head()
 relt = rd.load_rel_target()
-relt.head()
 
 topicsd = rd.load_topics_data()
-topicsd.head()
 topicst = rd.load_topics_target()
-topicst.head()
 
 whd = rd.load_wh_data()
-whd.head()
 wht = rd.load_wh_target()
-wht.head()
 
 ynd = rd.load_yn_data()
-ynd.head()
 ynt = rd.load_yn_target()
-ynt.head()
 
-#Concatenamos los ejemplos
+#Concatenamos los ejemplos en una misma tabla
 data = pd.concat([affirdd,condd,doubtqd,emphd,negd,reld,topicsd,whd,ynd], ignore_index=True)
+# print(data)
 
-#Quitamos la columna temporal
-data.drop('0.0',axis=1)      
+# test = rd.readAll()
+# print(test)
 
-#Concatenamos las etiquetas
+#Quitamos la columna de tiempo de los frames
+data.drop('0.0',axis=1)
+
+#Concatenamos las etiquetas en una tabla
 y = pd.concat([affirdt,condt,doubtqt,empht,negt,relt,topicst,wht,ynt], ignore_index=True)
+# print(y)
+ytest = rd.readAll(targets=True)
+# print(ytest)
 
 
 
--- readData.py
+++ readData.py
@@ -1,6 +1,35 @@
 # Definimos las funciones de lectura de datos
 import pandas as pd
 
+from os import listdir
+from os.path import isfile, join
+def readAll(user="All",targets=False,path="./datos/"):
+    onlyfiles = [f for f in listdir(path) if  isfile(join(path,f))]
+    onlyfiles.sort()
+    data = []
+    searching_for = "datapoints"
+    if targets:
+        searching_for = "targets"
+
+    if user=="All":
+        for c,f in enumerate(onlyfiles):
+            if targets: c=c-1
+            if f.find(searching_for)!=-1 and f.find("a")==0:
+                print(c,"Adding:",f)
+                data.append(pd.read_csv(path+f,header=0,delimiter=" "))
+                f = "b" + f[1:]
+                print(c,"Adding:", f)
+                data.append(pd.read_csv(path+f,header=0,delimiter=" "))
+                print("-----:")
+    else:
+        for f in onlyfiles:
+            if f.find(searching_for)!=-1 and f.find(user)==0:
+                print("Adding:",f)
+                data.append(pd.read_csv(path+f,header=0,delimiter=" "))
+
+    data = pd.concat(data,ignore_index=True)
+    return data
+
 def load_affirm_data(user="b",path="./datos/"):
     return pd.read_csv(path+user+"_affirmative_datapoints.txt",header=0,delimiter=" ")
 def load_affirm_target(user="b",path="./datos/"):
@@ -45,4 +74,3 @@ def load_yn_data(user="b",path="./datos/"):
     return pd.read_csv(path+user+"_yn_question_datapoints.txt",header=0,delimiter=" ")
 def load_yn_target(user="b",path="./datos/"):
     return pd.read_csv(path+user+"_yn_question_targets.txt",header=0,delimiter=" ")
-
